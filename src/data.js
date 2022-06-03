7159163373e93154cffe0869a14a4ce0b97d78cf
Damilare Abogunrin <43679376+St-Thierry@users.noreply.github.com>
2022-06-03 01:56:03 +0100

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    id: 1,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "http://lama.dev",
+  },
+];
